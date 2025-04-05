3f4158601ab5038618c5c77414eab4c4687a0177
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-05 11:09:20 -0400

    Add files via upload

---
+++ Desk/delete_javelin.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+cd
+cd .config/autostart
+sudo rm JAVELIN.sh.desktop
+cd
+sudo rm -r javelin javenv
+cd Desktop
+sudo rm -r Desk edit_javelin.sh start_javelin.sh delete_javelin.sh
+cd
+exit
