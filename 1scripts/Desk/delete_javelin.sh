3c4fbd409152b7551f5c5e74c71c9f636a122b12
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-09 12:06:29 -0400

    Add files via upload

---
+++ 1scripts/Desk/delete_javelin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+konsole -e "cd
+cd .config/autostart
+sudo rm JAVELIN.sh.desktop
+cd
+sudo rm -r javelin javenv
+cd Desktop
+sudo rm -r Desk edit_javelin.sh start_javelin.sh delete_javelin.sh
+cd
+exit"
---
+++ 1scripts/Desk/start_javelin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Open a terminal and run the JAVELIN.sh script
+cd
+cd javelin
+konsole --qwindowgeometry 990x300 -e "./JAVELIN.sh" &
+read -p
