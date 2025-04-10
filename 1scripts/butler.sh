16e58b5080a51c084148635ed9f913d5904c7932
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-10 16:42:02 -0400

    Update butler.sh

--- 1scripts/butler.sh
+++ 1scripts/butler.sh
@@ -6,12 +6,7 @@ directory=$(pwd)
 date=$(date)
 
 echo "Butler: Hello $name!"
-echo "$age years old"
-echo ""
-sleep 1
-echo "How we feeling today?!"
-echo ""
-sleep 1
+echo "$age years old, bday is coming up in this many days..."
 echo "You're logged in as $user,"
 echo "Directory: $directory"
 echo "Date: $date"
@@ -21,64 +16,63 @@ echo "$directory"
 echo ""
 echo "What to do?"
 echo "Call Mom (1) | Edit asem.s file (2) | Compile, assemble, run asem (3)"
-echo "Run javelin (4) | Edit javelin (5) | run deskinstall.sh (6)"
+echo "Display frontend (4) | Edit javelin (5) | Run deskinstall.sh (6)"
 echo "Upload javelin to USB 'Lexar' (7) | Git clone javelin locally (8) | Exit javelin (x)"
 read opt
 if [[ $opt == "1" ]]; then
 		echo "Call Mom..." 
 		sleep 1
 		./mom.sh $name $age
-
 	elif [[ $opt == "2" ]]; then
-		vim asem.s
-
+		cd assembly
+  		vim asem.s
+		cd ..
 	elif [[ $opt == "3" ]]; then
-		sleep 1
+		cd assembly
 		as asem.s -o asem.o
 		gcc -o asem asem.o -nostdlib -static
 		sleep 1
 		echo ""
 		./asem
 		echo ""
+  		cd ..
 		read
-
 	elif [[ $opt == "4" ]]; then
-		echo "Open javelin web"
+		cd ..
+    		echo "Open javelin web"
 		echo "Open GUI's"
-		python3 app_gui.py & python3 toolbar_gui.py & chromium -e templates/index.html
-		
+		python3 frontend/gui/app_gui.py & python3 frontend/gui/toolbar_gui.py & chromium -e index.html
+		sleep 1
+  		cd 1scripts
 	elif [[ $opt == "5" ]]; then
 		echo "Edit javelin..."
+		cd ..
 		sleep 1
-		python3 app_gui.py & python3 toolbar_gui.py & chromium -e "templates/index.html" & konsole -e --qwindowgeometry 525x400+50+50 "nano Desk/JAVELIN.sh deskinstall.sh butler.sh mom.sh intro.sh app_gui.py app.py toolbar_gui.py"
-		./butler.sh $name $age
-
+  		python3 frontend/gui/app_gui.py & python3 frontend/gui/toolbar_gui.py & chromium -e "index.html" & konsole -e --qwindowgeometry 525x400+50+50 "nano 1scripts/JAVELIN.sh 1scripts/deskinstall.sh 1scripts/butler.sh 1scripts/mom.sh 1scripts/intro.sh frontend/gui/app_gui.py backend/app/app.py frontend/gui/toolbar_gui.py"
+  		sleep 1
+    		cd 1scripts
+  		./deskinstall.sh
+    		sleep 1
+  		./butler.sh $name $age
 	elif [[ $opt == "6" ]]; then
 		./deskinstall.sh
 		echo "ran deskinstall.sh"
 		read
-
 	elif [[ $opt == "7" ]]; then
-		echo "Upload javelin to USB named Lexar..."
-		sleep 1
+		echo "Upload javelin to the USB 'Lexar'..."
 		cd
 		echo "arrived in home directory"
 		sleep 1
 		echo "copying javelin to usb"
 		sudo cp -r javelin /run/media/$USER/Lexar
 		echo "copied javelin directory to /run/media/$USER/Lexar"
-		cd javelin
-
+		cd javelin/1scripts
 	elif [[ $opt == "8" ]]; then
-		cd
-		cd Desktop/Desk
 		./gitcloning.sh $name $age
-
 	else
-		echo "Close Butler..."
+		echo "Closing Butler..."
 		sleep 1
 		exit
 	fi
-
 
 ./butler.sh $name $age
