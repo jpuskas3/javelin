bb13cf79b8c5a1dbf2305837b6e71141f41068e6
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-10 15:03:41 -0400

    Update mom.sh

--- 1scripts/mom.sh
+++ 1scripts/mom.sh
@@ -4,14 +4,14 @@ age=$2
 compliment=$3
 echo "Calling $name's mom"
 sleep 1
-echo "Uh uh"
-sleep 1
-echo "Oh wow"
+echo "uh uh"
+cd
 sleep 1
 echo "no way"
-cd templates
+cd javelin
 sleep 1
-echo "Can you do me a favor?"
+echo "can you do me a favor?"
+cd templates
 sleep 1
 echo ""
 echo "(1) Run javelin web w/ all HTML and CSS files in nano?"
@@ -25,40 +25,59 @@ read opt
 	
 if [[ $opt == "1" ]]; then
 	echo ""
-	chromium index.html & konsole -e --qwindowgeometry 525x400+50+50 "nano dash_networks.html dash_office.html dash_profile.html dash_settings.html index.html lab_actions.html lab_outputs.html labs.html lib_actions.html lib_volumes.html library.html map_actions.html map_key.html map_overlays.html maps.html mobile.html mobile_config.html mobile_download.html project.html javelin.css javelin.html"
+	chromium ../index.html & konsole -e --qwindowgeometry 525x400+50+50 "nano dash_networks.html dash_office.html dash_profile.html dash_settings.html ../index.html lab_actions.html lab_outputs.html labs.html lib_actions.html lib_volumes.html library.html map_actions.html map_key.html map_overlays.html maps.html mobile.html mobile_config.html mobile_download.html project.html ../javelin.css javelin.html"
 	echo ""
+ 	cd
+	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 elif [[ $opt == "2" ]]; then
 	echo ""
-	konsole -e --qwindowgeometry 525x400+50+50 "nano dash_networks.html dash_office.html dash_profile.html dash_settings.html project.html javelin.html javelin.css"
+	konsole -e --qwindowgeometry 525x400+50+50 "nano dash_networks.html dash_office.html dash_profile.html dash_settings.html project.html javelin.html ../javelin.css"
 	echo ""
+ 	cd
+   	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 elif [[ $opt == "3" ]]; then
 	echo ""
-	konsole -e --qwindowgeometry 525x400+50+50 "nano labs.html lab_outputs.html lab_actions.html project.html javelin.css"
+	konsole -e --qwindowgeometry 525x400+50+50 "nano labs.html lab_outputs.html lab_actions.html project.html ../javelin.css"
 	echo ""
+  	cd
+   	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 elif [[ $opt == "4" ]]; then
 	echo ""
-	konsole -e --qwindowgeometry 525x400+50+50 "nano library.html lib_volumes.html lab_actions.html project.html javelin.css"
+	konsole -e --qwindowgeometry 525x400+50+50 "nano library.html lib_volumes.html lab_actions.html project.html ../javelin.css"
 	echo ""
+  	cd
+   	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 elif [[ $opt == "5" ]]; then
 	echo ""
-	konsole -e --qwindowgeometry 525x400+50+50 "nano mobile.html mobile_download.html mobile_config.html project.html javelin.css"
-	echo "" 
+	konsole -e --qwindowgeometry 525x400+50+50 "nano mobile.html mobile_download.html mobile_config.html project.html ../javelin.css"
+	echo ""
+  	cd
+   	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 elif [[ $opt == "6" ]]; then
 	echo ""
-	konsole -e --qwindowgeometry 525x400+50+50 "nano maps.html map_actions.html map_key.html map_overlays.html javelin.html javelin.css"
+	konsole -e --qwindowgeometry 525x400+50+50 "nano maps.html map_actions.html map_key.html map_overlays.html javelin.html ../javelin.css"
 	echo ""
+  	cd
+   	cd javelin/1scripts
+	./mom.sh $name $age $compliment
 else
 	echo "Something else? (y/n)"
 	read opt
 	if [[ $opt == "y" ]]; then
+ 		cd
+   		cd javelin/1scripts
 		./mom.sh $name $age $compliment
 	else
-		echo "No Thanks! Bye Mom"
-		cd ..
-		sleep 1
-		./butler.sh $name $age $compliment
+		echo "No Thanks! See ya"
+		cd
+  		cd javelin/1scripts
 	fi
 fi
-cd ..
+
 echo "Directory: $directory"
 ./butler.sh $name $age $compliment
