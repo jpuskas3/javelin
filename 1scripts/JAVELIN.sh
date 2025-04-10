18d3078ab1a96e4d1e33235d4bd2ba14804b9ed3
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-09 21:28:59 -0400

    Update JAVELIN.sh

--- 1scripts/JAVELIN.sh
+++ 1scripts/JAVELIN.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd
 source javenv/bin/activate
-cd javelin
 
+cd javelin
 xterm -geometry 83x21+518+419 -e "bash -c 'source $HOME/javelin/1scripts/intro.sh; exec bash'" &
 sleep 1  # Wait for the Konsole window to initialize
 python3 login_gui.py
@@ -14,7 +14,7 @@ if [[ $(docker ps -aq -f name=flask-app) ]]; then
     docker rm flask-app
 fi
 echo "Building and starting a new container..."
-docker build -t flask-app .
+docker build -f 3docker/Dockerfile -t flask-app .
 docker run -d --name flask-app -p 80:80 -p 5000:5000 flask-app
 
 python3 frontend/gui/toolbar_gui.py &
