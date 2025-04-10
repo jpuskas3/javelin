4fbab0bdfe1c3ade0dae115d909eca9139375c04
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-10 14:28:07 -0400

    Update gitcloning.sh

--- 1scripts/gitcloning.sh
+++ 1scripts/gitcloning.sh
@@ -5,15 +5,16 @@ age=$2
 compliment=$3
 
 echo "this is the git cloning/commit page"
-echo "javelin will be erased and reinstalled from git"
-echo "or this local javelin will be committed to the GitHub jpuskas3 javelin repository"
-echo "clone(1) commit(2) or go back(x)?"
+echo "javelin will be erased and reinstalled from git unless committed"
+echo ".................."
+echo ".................."
+echo "clone javelin(1) | commit to main (2) | clone javelin2 (3) | commit to test(4) | back(x)?"
 read opt
 if [[ $opt == 1 ]]; then
         cd
         sudo rm -r javelin
         git clone https://github.com/jpuskas3/javelin.git
-        cd javelin
+        cd javelin/1scripts
         sudo chmod +x deskinstall.sh
         ./deskinstall.sh $name $age $compliment
         ./butler.sh $name $age $compliment
@@ -22,6 +23,23 @@ if [[ $opt == 1 ]]; then
         git add
         git commit
 
+    elif [[ $opt == 3 ]]; then
+        cd
+        cd Desktop
+        sudo rm -r javelin
+        echo "how to clone from test branch?"
+        echo "not doing it yet"
+        read
+# git clone https://github.com/jpuskas3/javelin.git
+    
+    elif [[ $opt == 4 ]]; then
+        cd
+        cd Desktop/javelin
+        echo "how to commit to test vs main?"
+        echo "not doing it yet"
+        read
+# git add
+# git commit
     else
         ./butler.sh $name $age $compliment
     fi
