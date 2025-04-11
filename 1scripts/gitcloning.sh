0d7053ecf3b96b9671e13703477803aa8e3317b1
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2025-04-10 23:05:22 -0400

    Update gitcloning.sh

--- 1scripts/gitcloning.sh
+++ 1scripts/gitcloning.sh
@@ -8,7 +8,7 @@ echo "this is the git cloning/commit page"
 echo "javelin will be erased and reinstalled from git unless committed"
 echo ".................."
 echo ".................."
-echo "clone javelin(1) | commit to main (2) | clone javelin2 (3) | commit to test(4) | back(x)?"
+echo "clone javelin(1) | commit to main (2) | clone javelin2 (3) | commit to test(4) | nevermind(x)?"
 read opt
 if [[ $opt == 1 ]]; then
         cd
@@ -17,28 +17,43 @@ if [[ $opt == 1 ]]; then
         cd javelin/1scripts
         sudo chmod +x deskinstall.sh
         ./deskinstall.sh $name $age $compliment
-        ./butler.sh $name $age $compliment
-
     elif [[ $opt == 2 ]]; then
-        echo "not doing commits yet"
-# git add
-# git commit
-
+        cd ..
+        git checkout main
+        git add .
+        git commit -m "updated from local"
+        git push https://github.com/jpuskas3/javelin.git main
+echo "Merge a pull request (from the command line, typically after testing):
+ To merge a pull request, you would fetch the pull request into your local repo and then merge it:
+git fetch https://github.com/your-username/your-repo.git pull/<PR-number>/head:test
+git checkout test
+git merge test"
     elif [[ $opt == 3 ]]; then
         cd
         cd Desktop
         sudo rm -r javelin
         git clone --branch test https://github.com/jpuskas3/javelin.git
-    
+        cd
+        cd javelin/1scripts
     elif [[ $opt == 4 ]]; then
         cd
         cd Desktop/javelin
-        echo "how to commit to test vs main?"
-        echo "not doing commits yet"
-        read
-# git add
-# git commit
-    
+        git checkout test
+        git add .
+        git commit -m "updated from local desktop"
+        git push https://github.com/jpuskas3/javelin.git test
+        cd
+        cd javelin/1scripts
     else
-        ./butler.sh $name $age $compliment
+        echo "./butler.sh $name $age $compliment"
+fi
+
+echo "anything else in gitcloning? (y/n)"
+read opt
+
+if [[ $opt == y ]]; then
+        ./gitcloning.sh
+    else
+        echo "exiting gitcloning..."
+        exit
 fi
